ac3eaa3f22219957e9a3f8aa03a393f59ea506e6
汪训龙 <476699089@qq.com>
2023-06-19 15:42:14 +0800

    跨域设置

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ let users = [
     {id:2, name: 'dalong2'}
 ]
 http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*') //任意域名都可以访问,或者基于我请求头里面的域
+    res.setHeader('Access-Control-Allow-Credentials', true)//允许后端发送cookie
+    res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')//允许支持的请求方式
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type')//设置请求头格式和类型
     if (req.url == '/api/users') {
         res.end(JSON.stringify(users))
     } else {
